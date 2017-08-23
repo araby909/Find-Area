f7badbe88cef4167c62f84a3fff7d22e9cae2a45
araby909 <30875419+araby909@users.noreply.github.com>
2017-08-23 08:50:48 -1000

    Add files via upload

---
+++ width,height.js
@@ -0,0 +1,20 @@
+function findArea(width,height){
+ window.alert(width * height);
+}
+
+function calculateArea(width,height){
+var area = width * height;
+  return area;
+  }
+  var wallOne = calculateArea(3,5);
+  window.alert(wallOne);
+  
+  
+function calculateArea(width,height){
+var area = width * height;
+  return area;
+  }
+  var width = prompt("What is the width?")
+  var height = prompt("what is the height?")
+  var wallOne = calculateArea(width,height);
+  window.alert(wallOne);
