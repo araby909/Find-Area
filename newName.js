03f7c9910a6de6663aa54149efab192310de4e34
araby909 <30875419+araby909@users.noreply.github.com>
2017-08-28 08:45:04 -1000

    Add files via upload

---
+++ newName.js
@@ -0,0 +1,9 @@
+var newName = prompt ("What is his new first name?")
+
+var Logic = {
+  firstName:"Bobby",
+  lastName:"Tarantino",
+  grade:"11",
+  age:"27"
+};
+window.alert(Logic.firstName = newName);
