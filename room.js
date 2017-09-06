55440ca11f9d6dbbd71228d36ee8ffd0878a6472
araby909 <30875419+araby909@users.noreply.github.com>
2017-09-06 09:18:30 -1000

    Update room.js

--- room.js
+++ room.js
@@ -84,3 +84,30 @@ window.alert(room[lowercase]);
   }
  }
 }
+
+
+
+
+
+// tell user how much tries left
+
+var room = {
+ classroom: "F103",
+ students: 18,
+};
+
+var userInput = prompt("classroom or students?");
+var lowercase = userInput.toLowerCase();
+if (lowercase === "students" || lowercase === "classroom"){
+window.alert(room[lowercase]);
+}
+else {
+ for(i = 1; i < 3; i++) {
+ var userInput = prompt("classroom or students? You have tried " + i + " times");
+var lowercase = userInput.toLowerCase();
+if (lowercase === "students" || lowercase === "classroom"){
+ i = 3;
+window.alert(room[lowercase]);
+  }
+ }
+}
