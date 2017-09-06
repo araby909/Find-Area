0caed58541a71f5fea3d782680c546cd7ec9b6ef
araby909 <30875419+araby909@users.noreply.github.com>
2017-09-06 09:12:07 -1000

    Update room.js

--- room.js
+++ room.js
@@ -62,3 +62,25 @@ else {
 askAgain();
  }
 }
+
+//stop after 4 attempts
+var room = {
+ classroom: "F103",
+ students: 18,
+};
+
+var userInput = prompt("classroom or students?");
+var lowercase = userInput.toLowerCase();
+if (lowercase === "students" || lowercase === "classroom"){
+window.alert(room[lowercase]);
+}
+else {
+ for(i = 0; i < 3; i++) {
+ var userInput = prompt("classroom or students?");
+var lowercase = userInput.toLowerCase();
+if (lowercase === "students" || lowercase === "classroom"){
+ i = 3;
+window.alert(room[lowercase]);
+  }
+ }
+}
