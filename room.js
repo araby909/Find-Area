c19e003439063f553d750c21497272f65fbb57d6
araby909 <30875419+araby909@users.noreply.github.com>
2017-09-06 08:33:21 -1000

    Update room.js

--- room.js
+++ room.js
@@ -9,3 +9,21 @@ var userInput = prompt("classroom or students?");
 var lowercase = userInput.toLowerCase();
 
 window.alert(room[lowercase])
+
+
+//if else statment
+
+var room = {
+ classroom: "F103",
+ students: 18,
+};
+
+var userInput = prompt("classroom or students?");
+
+var lowercase = userInput.toLowerCase();
+
+if (lowercase == "students" || lowercase === "classroom")
+
+(window.alert(room[lowercase]))
+
+else(window.alert("Please enter classrom or students"))
